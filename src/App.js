cf67eff16039ddeb02518236d3eab4c8e7b6b395
Kailash1233 <kailash61203@gmail.com>
2023-09-18 15:03:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import TextBlock from './textBlock';
 import './App.css';
+import Moon from './images/Moon.jpg'
+import Moon1 from './images/Moon1.jpg'
+import BlackMoon from './images/BlackMoon.jpg'
+import Kailu from './images/Kailu.png'
+import Man from './images/man_on_mountain.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Parallax pages={1.9} style={{ top: '0', left: '0' }} class="animation">
+        {/* <ParallaxLayer offset={0} speed={0.25}>
+          <div class="animation_layer parallax" id="artback"></div>
+        </ParallaxLayer> */}
+        {/* <ParallaxLayer offset={0} speed={0.3}>
+          <div class="animation_layer parallax" id="mountain"></div>
+        </ParallaxLayer> */}
+        <ParallaxLayer offset={0} speed={-0.1}>
+          <div class="animation_layer parallax" id="logoland"><center><h1 id='name'>Kailash G</h1></center></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.3}>
+          <div class="animation_layer parallax" id="jungle1"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.35}>
+          <div class="animation_layer parallax" id="jungle2"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.5}>
+          <div class="animation_layer parallax" id="jungle3"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.45}>
+          <div class="animation_layer parallax" id="jungle4"></div>
+        </ParallaxLayer>
+        {/* <ParallaxLayer offset={0} speed={0.40}>
+          <div class="animation_layer parallax" id="manonmountain"></div>
+        </ParallaxLayer> */}
+        <ParallaxLayer offset={0} speed={0.35}>
+          <div class="animation_layer parallax" id="jungle5"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0.9} speed={0.25}>
+          <TextBlock />
+        </ParallaxLayer>
+      </Parallax>
+
+
+
+
+
+      {/* <Parallax pages ={2} ref={ref}>
+          <ParallaxLayer 
+            offset={0} 
+            speed={0.35} 
+            style = {{
+            backgroundImage: `url(${Moon1})`,
+            backgroundSize: 'cover',
+          }}
+          >
+            <div class="animation_layer parallax"></div>
+            <center>
+            <h1 style={{
+              color:'white'}}>Hi, I am Kailash</h1>
+            </center>
+          </ParallaxLayer>
+              
+          <ParallaxLayer offset={1} speed={0.25}>
+          <div class="animation_layer parallax" id="artback"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={1} speed={0.3}>
+          <div class="animation_layer parallax" id="mountain"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={1} speed={0.3}>
+          <div class="animation_layer parallax" id="jungle1"></div>
+        </ParallaxLayer>    
+          <ParallaxLayer 
+            offset={1} 
+            speed={0.15} 
+            factor={1} 
+            sticky= {{start: 0.9, end: 2.5}} 
+            onClick = {() => ref.current.scrollTo(1)}
+            style= {{
+            backgroundImage:`url(${Man})`,
+            backgroundSize: 'cover',
+            }}>  
+            <div class="animation_layer parallax"></div>
+          </ParallaxLayer>
+
+          <ParallaxLayer
+            offset={1}
+            speed= {0.35}
+          >
+            <div class="animation_layer parallax"></div>
+            <img src={Kailu}></img>
+          </ParallaxLayer>
+      </Parallax> */}
     </div>
   );
 }
---
+++ src/textBlock.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function TextBlock() {
+    return (
+        <div id="textblock">
+            <div id="textblock-container">
+                <h1 id="textblock-title">Hey There! This is Kailash</h1>
+                <p id="textblock-content">
+                A Passionate Web Developer<br/><br/>
+                Hello, I'm Kailash, a student at B.S. Abdur Rahman Crescent Institute of Science and Technology. I have a strong passion for exploring new technologies and expanding my knowledge in the field of Information Technology. <br/><br/>
+                In addition to my interest in new technologies, I am particularly enthusiastic about web development. I enjoy designing and building interactive and user-friendly websites.<br/><br/>
+                I strive to stay updated with the latest web development trends and techniques to create compelling online experiences.
+                </p>
+            </div>
+            <footer id='textblock-footer'>Created by Kailash&nbsp;<a id="textblock-devsense" href="https://www.youtube.com/@sapientcodes/">Sapient Codes</a></footer>
+        </div>
+    );
+}
+
+export default TextBlock;
